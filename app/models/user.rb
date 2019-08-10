a27ee8f7613a7163a57f9ad6855d021fd42c4a37
jacklin953107 <jacklin953107@gmail.com>
2019-08-10 21:17:08 +0800

    user can view commented restaurants on profile page

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable
   mount_uploader :avatar, AvatarUploader
   has_many :comments
+  has_many :restaurants, through: :comments
 
   def admin?
     self.role == "admin"
