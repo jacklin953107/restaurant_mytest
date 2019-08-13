52b9d7d4fe4c8cefc4d0ca7598bb02b7dc949206
jacklin953107 <jacklin953107@gmail.com>
2019-08-13 14:16:03 +0800

    setup self-referential relationships within users table through followships

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,6 +10,8 @@ class User < ApplicationRecord
   has_many :favorited_restaurants, through: :favorites, source: :restaurant
   has_many :likes, dependent: :destroy
   has_many :liked_restaurants, through: :likes, source: :restaurant
+  has_many :followships, dependent: :destroy
+  has_many :followings, through: :followships
 
   def is_liked?(restaurant)
     self.liked_restaurants.include?(restaurant)
